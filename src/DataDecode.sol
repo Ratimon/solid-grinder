74c0caa0e031270143aa196b7cad9cc312fbcafc
Ratimon <ratixoxo@gmail.com>
2023-07-05 20:55:18 +0700

    Build (./src): Adding ByteLib

---
+++ src/DataDecode.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BytesLib} from "@main/libraries/BytesLib.sol";
+
+abstract contract DataDecoding {
+    using BytesLib for bytes;
+
+}
