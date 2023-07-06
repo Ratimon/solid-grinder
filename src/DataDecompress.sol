bb5317393d17fbaac0312106601e950f30102662
Ratimon <ratixoxo@gmail.com>
2023-07-06 15:14:14 +0700

    Feat (./src): Adding interface for uniswapv2 example

--- src/DataDecompress.sol
+++ src/DataDecompress.sol
@@ -67,7 +67,6 @@ abstract contract DataDecompress {
 
         _newCursor = _cursor;
 
-
     }
 
 }
---
+++ src/examples/uniswapv2/UniswapV2Router02.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IUniswapV2Router02} from "@main/examples/uniswapv2/IUniswapV2Router02.sol";
+
+
+contract UniswapV2Router02 is IUniswapV2Router02 {
+
+}
