9f7f3796773455280b73ea0e386bcd2518082698
Ratimon <ratixoxo@gmail.com>
2023-07-06 15:43:32 +0700

    Feat (./src): Adding modified version of uniswapv2 router

---
+++ src/examples/uniswapv2/IUniswapV2Factory.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+interface IUniswapV2Factory {
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function allPairs(uint256) external view returns (address pair);
+    function allPairsLength() external view returns (uint256);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+}
