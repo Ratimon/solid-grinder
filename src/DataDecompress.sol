c5e4edda10c3e8756f857c37bf0827ae4dc520a0
Ratimon <ratixoxo@gmail.com>
2023-07-06 17:12:46 +0700

    Feat (./src): Adding deserialize template function for uint

--- src/DataDecompress.sol
+++ src/DataDecompress.sol
@@ -31,9 +31,9 @@ abstract contract DataDecompress {
         emit SetAutoRegisterAddressMapping(_enable);
     }
 
-    function toBytes(bytes32 _data) private pure returns (bytes memory) {
-        return abi.encodePacked(_data);
-    }
+    // function toBytes(bytes32 _data) private pure returns (bytes memory) {
+    //     return abi.encodePacked(_data);
+    // }
 
     function _lookupAddress_functionName1_24bits(
         bytes memory _data,
@@ -68,5 +68,47 @@ abstract contract DataDecompress {
         _newCursor = _cursor;
 
     }
+
+    function _deserializeAmount_functionName1(
+        bytes memory _data,
+        uint256 _cursor
+    )
+        internal
+        pure
+        returns (
+            uint256 _amount,
+            uint256 _newCursor
+        )
+    {
+        uint8 instruction = _data.toUint8(_cursor);
+        _cursor += 1;
+
+        if (instruction == 0) { // 64-bit, 18 (denominated in 1e18)
+            _amount = _data.toUint64(_cursor);
+            _cursor += 8;
+            
+        } else if (instruction == 1) { // 80-bit, 1.2m (denominated in 1e18)
+            _amount = _data.toUint80(_cursor);
+            _cursor += 10;
+
+        } else if (instruction == 2) { // 96-bit, 79.2b (denominated in 1e18)
+            _amount = _data.toUint96(_cursor);
+            _cursor += 12;
+
+        } else if (instruction == 3) { // 112-bit, 5,192mm (denominated in 1e18)
+            _amount = _data.toUint112(_cursor);
+            _cursor += 14;
+        
+        } else if (instruction == 4) { // zero
+            _amount = 0;
+
+        } else {
+            revert("DataDecompress: bad instruction");
+        }
+
+        _newCursor = _cursor;
+    }
+
+
 
 }
