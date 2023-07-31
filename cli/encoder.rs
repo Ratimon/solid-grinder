484055e8f703bd090a83d8ac6226c50dbbb24cad
Ratimon <ratixoxo@gmail.com>
2023-07-31 13:47:29 +0700

    Feat (./cli): Adding skeleton for encoder generator

---
+++ cli/encoder.rs
@@ -0,0 +1,52 @@
+use std::{fs, path::Path};
+
+use handlebars::Handlebars;
+
+use crate::types::ContractObject;
+
+pub fn generate_encoder(
+    contract: ContractObject,
+    contract_name: &str,
+    generated_directory: &str,
+) {
+    let mut handlebars = Handlebars::new();
+    handlebars.set_strict_mode(true);
+
+    let generated_name: String = format!("{}_DataEncoder.g.sol", contract_name);
+    handlebars
+        .register_template_string(
+            &generated_name,
+            include_str!("templates/Encoder.g.sol.hbs"),
+        )
+        .unwrap();
+
+    let folder_path_buf = Path::new(generated_directory).join("encoder");
+    let folder_path = folder_path_buf.to_str().unwrap();
+
+    fs::create_dir_all(folder_path).expect("create folder");
+
+    write_if_different(
+        &format!("{}/{}_Encoder.g.sol", folder_path, contract_name),
+        format!(
+            "{}",
+            handlebars
+                .render(&generated_name, &contract)
+                .unwrap()
+        ),
+    );
+    
+}
+    
+fn write_if_different(path: &String, content: String) {
+
+    let result = fs::read(path);
+    let same = match result {
+        Ok(existing) => String::from_utf8(existing).unwrap().eq(&content),
+        Err(_e) => false,
+    };
+
+    if !same {
+        println!("writing new files...");
+        fs::write(path, content).expect("could not write file");
+    }
+}
--- cli/main.rs
+++ cli/main.rs
@@ -21,18 +21,38 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Generate Encoder Helper with source found
+    // Generate Encoder Helper with source found
     GenDecoder(GenDecoderArgs),
+    GenEncoder(GenEncoderArgs),
 }
 
 #[derive(clap::Args)]
 struct GenDecoderArgs {
 
     #[arg(short, long)]
-    source: Option<String>,   // contracts/examples/uniswapv2/UniswapV2Router02.sol
+    source: Option<String>,          // contracts/examples/uniswapv2/UniswapV2Router02.sol
 
     #[arg(short, long)]
-    output: Option<String>,    // ./optimized
+    output: Option<String>,          // ./optimized
+
+    #[arg(short, long)]
+    contract_name: Option<String>,   // UniswapV2Router02
+
+    #[arg(short, long)]
+    function_name: Option<String>,   // addLiquidity
+
+    #[arg(short, long, value_parser, num_args = 1, value_delimiter = ' ')]
+    arg_bits: Vec<String>,
+}
+
+#[derive(clap::Args)]
+struct GenEncoderArgs {
+
+    #[arg(short, long)]
+    source: Option<String>,          // contracts/examples/uniswapv2/UniswapV2Router02.sol
+
+    #[arg(short, long)]
+    output: Option<String>,          // ./optimized
 
     #[arg(short, long)]
     contract_name: Option<String>,   // UniswapV2Router02
@@ -51,10 +71,27 @@ fn main() {
     match &cli.command {
         Some(command) => match command {
             Commands::GenDecoder(args) => {
-                gen_decoder(&cli.root, &args.source, &args.output, &args.contract_name, &args.function_name, &args.arg_bits)
+                gen_decoder(
+                    &cli.root,
+                    &args.source,
+                    &args.output,
+                    &args.contract_name,
+                    &args.function_name,
+                    &args.arg_bits
+                )
+            }
+            Commands::GenEncoder(args) => {
+                gen_encoder(
+                    &cli.root,
+                    &args.source,
+                    &args.output,
+                    &args.contract_name,
+                    &args.function_name,
+                    &args.arg_bits
+                )
             }
         },
-        None => test(),
+        None => no_match_command(),
     }
 }
 
@@ -91,6 +128,41 @@ fn gen_decoder(
     decoder::generate_decoder( contract, contract_name , generated_directory_path);
 }
 
-fn test() {
-    println!("'solid-grinder'")
+fn gen_encoder(
+    root: &Option<String>,
+    source: &Option<String>,
+    output: &Option<String>,
+    contract_name: &Option<String>,
+    function_name: &Option<String>,
+    arg_bits: &Vec<std::string::String>
+) {
+
+    let root_directory = root.as_deref().unwrap_or(".");
+    let source_directory = source.as_deref().unwrap_or("contracts/examples/uniswapv2/UniswapV2Router02.sol");
+    let contract_name = contract_name.as_deref().unwrap_or("UniswapV2Router02");
+    let function_name = function_name.as_deref().unwrap_or("addLiquidity");
+    //  fix this with test
+
+    let (successes, _): (Vec<_>, Vec<_>) = arg_bits.iter().partition_map(|i| match i.parse::<u16>() {
+        Ok(v) => Either::Left(v),
+        Err(_) => Either::Right(i.to_string()),
+    });
+
+    let bits: Result<Vec<u16>, String> = if successes.iter().all(|bit| bit % 8 == 0 && bit <= &256) {
+        Ok(successes)
+    } else {
+        Err("Some elements are not divisible by 8 or greater than 256.".to_string())
+    };
+
+    let contract: types::ContractObject = src_artifacts::get_contract(root_directory, source_directory, contract_name, function_name, bits.unwrap()).unwrap();
+    let generated_directory = output.as_deref().unwrap_or("optimized");
+    let generated_directory_path_buf = Path::new(root_directory).join(generated_directory);
+    let generated_directory_path = generated_directory_path_buf.to_str().unwrap();
+
+    encoder::generate_encoder( contract, contract_name , generated_directory_path);
+
+}
+
+fn no_match_command() {
+    println!("'no match command'")
 }
