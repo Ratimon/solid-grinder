558b9db14241f3ebfc4c8b57577eace6058ceb83
Ratimon <ratixoxo@gmail.com>
2023-08-10 15:24:17 +0700

    Build (./npm): Adding npm building process

---
+++ build-npm.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+const args = process.argv.slice(2);
+
+const {execFileSync} = require('child_process')
+const fs = require("fs");
+const stdio = ["inherit", "inherit", "inherit"];
+
+
+const version_regex = /version[\s]*=[\s]*"(.*?)"/gm;
+const cargo_toml = fs.readFileSync("Cargo.toml", "utf-8");
+const version = [...version_regex.exec(cargo_toml)][1];
+const pkg_version = (args[0] === 'publish:npm' || args[0] === 'npm:final') ? version : version + '-rc.1';
+
+// ------------------------------------------------------------------------------------------------
+// package.json
+// ------------------------------------------------------------------------------------------------
+const package_json = fs.readFileSync("npm/package.json", "utf-8");
+fs.writeFileSync("package.json", package_json.replace("__VERSION__", pkg_version))
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// install.js
+// ------------------------------------------------------------------------------------------------
+const install_js = fs.readFileSync("npm/install.js", "utf-8");
+fs.writeFileSync("install.js", install_js.replace("__VERSION__", version))
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// run.js
+// ------------------------------------------------------------------------------------------------
+const run_js = fs.readFileSync("npm/run.js", "utf-8");
+fs.writeFileSync("run.js", run_js.replace("__VERSION__", version));
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// binary.js
+// ------------------------------------------------------------------------------------------------
+const binary_js = fs.readFileSync("npm/binary.js", "utf-8");
+fs.writeFileSync("binary.js", binary_js.replace("__VERSION__", version));
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// binary.js: just copy
+// ------------------------------------------------------------------------------------------------
+const binary_install = fs.readFileSync("npm/binary-install.js", "utf-8");
+fs.writeFileSync("binary-install.js", binary_install);
+// ------------------------------------------------------------------------------------------------
+
+
+if (args[0] === 'npm:final') {
+    
+} else if (args[0] === 'publish:npm') {
+    execFileSync("npm", ["publish"], {stdio});
+} else if (args[0] === 'publish') {
+    execFileSync("cargo", ["install", "cargo-release"], {stdio});
+    execFileSync("cargo", ["release", "--execute"], {stdio});
+    execFileSync("npm", ["publish", "--tag", "rc"], {stdio});
+} else {
+    execFileSync("cargo", ["build"], {stdio});
+}
---
+++ npm/binary.js
@@ -0,0 +1,78 @@
+const { Binary } = require("./binary-install");
+const os = require("os");
+const {join} = require("path");
+
+const error = msg => {
+  console.error(msg);
+  process.exit(1);
+};
+
+const { version: actualVersion, name, repository } = require("./package.json");
+const version = "0.0.0";
+
+const supportedPlatforms = [
+  {
+    TYPE: "Windows_NT",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-pc-windows-msvc",
+    BINARY_NAME: "solid-grinder.exe"
+  },
+  {
+    TYPE: "Linux",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-unknown-linux-musl",
+    BINARY_NAME: "solid-grinder"
+  },
+  {
+    TYPE: "Darwin",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-apple-darwin",
+    BINARY_NAME: "solid-grinder"
+  },
+  {
+    TYPE: "Darwin",
+    ARCHITECTURE: "arm64",
+    RUST_TARGET: "aarch64-apple-darwin",
+    BINARY_NAME: "solid-grinder"
+  }
+];
+
+const getPlatformMetadata = () => {
+  const type = os.type();
+  const architecture = os.arch();
+
+  for (let supportedPlatform of supportedPlatforms) {
+    if (
+      type === supportedPlatform.TYPE &&
+      architecture === supportedPlatform.ARCHITECTURE
+    ) {
+      return supportedPlatform;
+    }
+  }
+
+  error(
+    `Platform with type "${type}" and architecture "${architecture}" is not supported by ${name}.\nYour system must be one of the following:\n\n${JSON.stringify(supportedPlatforms, null, 2)}`
+  );
+};
+
+const getBinary = () => {
+  const platformMetadata = getPlatformMetadata();
+  // the url for this binary is constructed from values in `package.json`
+  const url = `${repository.url}/releases/download/v${version}/${name}_v${version}_${platformMetadata.RUST_TARGET}.tar.gz`;
+  return new Binary(platformMetadata.BINARY_NAME, url, {installDirectory: join(__dirname, "bin")});
+};
+
+const run = () => {
+  const binary = getBinary();
+  binary.run();
+};
+
+const install = () => {
+  const binary = getBinary();
+  binary.install();
+};
+
+module.exports = {
+  install,
+  run
+};
