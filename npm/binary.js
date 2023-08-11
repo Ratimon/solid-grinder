c63c12a5ed03ea7c7091642197c3016aee130f0d
Ratimon <ratixoxo@gmail.com>
2023-08-11 19:14:24 +0700

    version 0.0.1

--- npm/binary.js
+++ npm/binary.js
@@ -8,7 +8,7 @@ const error = msg => {
 };
 
 const { version: actualVersion, name, repository } = require("./package.json");
-const version = "0.0.0";
+const version = "0.0.1";
 
 const supportedPlatforms = [
   {
