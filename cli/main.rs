779e923890baacd1f4a41e1c48aef5f0ad3e462c
Ratimon <ratixoxo@gmail.com>
2023-08-12 14:30:13 +0700

    Refactor & Build(./cli): v0.0.2

--- cli/main.rs
+++ cli/main.rs
@@ -3,6 +3,8 @@ use itertools::{Either, Itertools};
 
 use clap::{Parser, Subcommand};
 
+use crate::types::ContractObject;
+
 pub mod encoder;
 pub mod decoder;
 pub mod src_artifacts;
@@ -103,29 +105,8 @@ fn gen_decoder(
     function_name: &Option<String>,
     arg_bits: &Vec<std::string::String>
 ) {
-    let root_directory = root.as_deref().unwrap_or(".");
-    let source_directory = source.as_deref().unwrap_or("contracts/examples/uniswapv2/UniswapV2Router02.sol");
-    let contract_name = contract_name.as_deref().unwrap_or("UniswapV2Router02");
-    let function_name = function_name.as_deref().unwrap_or("addLiquidity");
-    //  fix this with test
-
-    let (successes, _): (Vec<_>, Vec<_>) = arg_bits.iter().partition_map(|i| match i.parse::<u16>() {
-        Ok(v) => Either::Left(v),
-        Err(_) => Either::Right(i.to_string()),
-    });
-
-    let bits: Result<Vec<u16>, String> = if successes.iter().all(|bit| bit % 8 == 0 && bit <= &256) {
-        Ok(successes)
-    } else {
-        Err("Some elements are not divisible by 8 or greater than 256.".to_string())
-    };
-
-    let contract: types::ContractObject = src_artifacts::get_contract(root_directory, source_directory, contract_name, function_name, bits.unwrap()).unwrap();
-    let generated_directory = output.as_deref().unwrap_or("optimized");
-    let generated_directory_path_buf = Path::new(root_directory).join(generated_directory);
-    let generated_directory_path = generated_directory_path_buf.to_str().unwrap();
-
-    decoder::generate_decoder( contract, contract_name , generated_directory_path);
+    let (contract, name, generated_directory_path) = get_data(root, source, output, contract_name, function_name, arg_bits);
+    decoder::generate_decoder( contract, name , &generated_directory_path);
 }
 
 fn gen_encoder(
@@ -137,6 +118,43 @@ fn gen_encoder(
     arg_bits: &Vec<std::string::String>
 ) {
 
+    let (contract, name, generated_directory_path) = get_data(root, source, output, contract_name, function_name, arg_bits);
+
+    // let root_directory = root.as_deref().unwrap_or(".");
+    // let source_directory = source.as_deref().unwrap_or("contracts/examples/uniswapv2/UniswapV2Router02.sol");
+    // let contract_name = contract_name.as_deref().unwrap_or("UniswapV2Router02");
+    // let function_name = function_name.as_deref().unwrap_or("addLiquidity");
+    // //  fix this with test
+
+    // let (successes, _): (Vec<_>, Vec<_>) = arg_bits.iter().partition_map(|i| match i.parse::<u16>() {
+    //     Ok(v) => Either::Left(v),
+    //     Err(_) => Either::Right(i.to_string()),
+    // });
+
+    // let bits: Result<Vec<u16>, String> = if successes.iter().all(|bit| bit % 8 == 0 && bit <= &256) {
+    //     Ok(successes)
+    // } else {
+    //     Err("Some elements are not divisible by 8 or greater than 256.".to_string())
+    // };
+
+    // let contract: types::ContractObject = src_artifacts::get_contract(root_directory, source_directory, contract_name, function_name, bits.unwrap()).unwrap();
+    // let generated_directory = output.as_deref().unwrap_or("optimized");
+    // let generated_directory_path_buf = Path::new(root_directory).join(generated_directory);
+    // let generated_directory_path = generated_directory_path_buf.to_str().unwrap();
+
+    encoder::generate_encoder( contract, name , &generated_directory_path);
+
+}
+
+fn get_data<'a>(
+    root: &Option<String>,
+    source: &Option<String>,
+    output: &Option<String>,
+    contract_name: &'a Option<String>,
+    function_name: &Option<String>,
+    arg_bits: &Vec<std::string::String>
+) -> (ContractObject,&'a str, String) {
+
     let root_directory = root.as_deref().unwrap_or(".");
     let source_directory = source.as_deref().unwrap_or("contracts/examples/uniswapv2/UniswapV2Router02.sol");
     let contract_name = contract_name.as_deref().unwrap_or("UniswapV2Router02");
@@ -157,10 +175,9 @@ fn gen_encoder(
     let contract: types::ContractObject = src_artifacts::get_contract(root_directory, source_directory, contract_name, function_name, bits.unwrap()).unwrap();
     let generated_directory = output.as_deref().unwrap_or("optimized");
     let generated_directory_path_buf = Path::new(root_directory).join(generated_directory);
-    let generated_directory_path = generated_directory_path_buf.to_str().unwrap();
+    let generated_directory_path = generated_directory_path_buf.to_str().unwrap().to_string();
 
-    encoder::generate_encoder( contract, contract_name , generated_directory_path);
-
+    (contract, contract_name, generated_directory_path)
 }
 
 fn no_match_command() {
