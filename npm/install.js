d6f91007469e9ff88393ff35bbe081cbefa432ab
Ratimon <ratixoxo@gmail.com>
2024-04-06 23:38:22 +0700

    Fix (./npm): import required dependency

--- npm/install.js
+++ npm/install.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-
+const fs = require("fs");
 const { install } = require("./binary");
+
 install();
 
 // ------------------------------------------------------------------------------------------------
