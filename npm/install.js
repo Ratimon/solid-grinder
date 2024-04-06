1d1b1caecbd27d68423f77ca9fcfbb5624faedfe
Ratimon <ratixoxo@gmail.com>
2024-04-07 00:23:49 +0700

    Fix (./npm): fixing creating file error

--- npm/install.js
+++ npm/install.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fs = require("fs");
+const path = require("path");
 const { install } = require("./binary");
 
 install();
@@ -8,14 +9,29 @@ install();
 // templates: just copy
 // ------------------------------------------------------------------------------------------------
 const solc_0_7_decoder = fs.readFileSync("cli/templates/solc_0_7/Decoder.g.sol.hbs", "utf-8");
-fs.writeFileSync("../../templates/solc_0_7/Decoder.g.sol.hbs", solc_0_7_decoder);
+let targetDir = path.join(__dirname, "/../../templates/solc_0_7/");
+if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+}
+fs.writeFileSync(path.join(targetDir, "Decoder.g.sol.hbs"), solc_0_7_decoder);
 
 const solc_0_7_encoder = fs.readFileSync("cli/templates/solc_0_7/Encoder.g.sol.hbs", "utf-8");
-fs.writeFileSync("../../templates/solc_0_7/Encoder.g.sol.hbs", solc_0_7_encoder);
+targetDir = path.join(__dirname, "/../../templates/solc_0_7/");
+if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+}
+fs.writeFileSync(path.join(targetDir, "Encoder.g.sol.hbs"), solc_0_7_encoder);
 
 const solc_0_8_decoder = fs.readFileSync("cli/templates/solc_0_8/Decoder.g.sol.hbs", "utf-8");
-fs.writeFileSync("../../templates/solc_0_8/Decoder.g.sol.hbs", solc_0_8_decoder);
+targetDir = path.join(__dirname, "/../../templates/solc_0_8/");
+if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+}
+fs.writeFileSync(path.join(targetDir, "Decoder.g.sol.hbs"), solc_0_8_decoder);
 
 const solc_0_8_encoder = fs.readFileSync("cli/templates/solc_0_8/Encoder.g.sol.hbs", "utf-8");
-fs.writeFileSync("../../templates/solc_0_8/Encoder.g.sol.hbs", solc_0_8_encoder);
-// ------------------------------------------------------------------------------------------------
+targetDir = path.join(__dirname, "/../../templates/solc_0_8/");
+if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir, { recursive: true });
+}
+fs.writeFileSync(path.join(targetDir, "Encoder.g.sol.hbs"), solc_0_8_encoder);
